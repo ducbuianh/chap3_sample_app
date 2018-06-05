221cbb823ef5bf4a63b2995d4ca6599c68d95b51
ducba <duc.buianh@pixta.co.jp>
2018-06-05 17:10:33 +0700

    fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ApplicationRecord
   before_save { email.downcase! }
-  validates :name, presence: true, length: { 
-                                      maximum: Constants::USERNAME_MAX_LENGTH }
+  validates :name, presence: true,
+                   length: { maximum: Constants::USERNAME_MAX_LENGTH }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 
-                                                   Constants::EMAIL_MAX_LENGTH },
+  validates :email, presence: true,
+                    length: { maximum: Constants::EMAIL_MAX_LENGTH },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    if ActiveModel::SecurePassword.min_cost
+      cost = BCrypt::Engine::MIN_COST
+    else
+      cost = BCrypt::Engine.cost
+    end
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
